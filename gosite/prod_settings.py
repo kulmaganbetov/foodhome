c040f7736124dc7a2ab843a96879f9f561ab1768
kulmaganbetov <nezoxplay555@gmail.com>
2024-05-22 01:45:13 +0500

    first commit

---
+++ gosite/prod_settings.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+SECRET_KEY = '*+uid(@$^dg8)1bs)*aasdsgfhsdas3aaea(0_=n@c6*xtaasdsumo'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['foodhome.kz', '185.22.64.19']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'gositedb',
+#         'USER': 'medet',
+#         'PASSWORD': 'dostar1996',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
